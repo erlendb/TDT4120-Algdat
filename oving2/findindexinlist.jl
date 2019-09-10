e4f90939ec1931fc69d9165b1dc2ecdefdea8e65
Erlend Blomseth <erlend@elendig.no>
2019-09-10 19:47:28 +0200

    2a uten testkode, kun funksjon

--- oving2/findindexinlist.jl
+++ oving2/findindexinlist.jl
@@ -1,22 +1,3 @@
-
-mutable struct Node
-    next::Union{Node, Nothing} # next kan peke på et Node-objekt eller ha verdien nothing.
-    value::Int
-end
-
-function createlinkedlist(length)
-    # Creates the list starting from the last element
-    # This is done so the last element we generate is the head
-    child = nothing
-    node = nothing
-
-    for i in 1:length
-        node = Node(child, rand(1:800))
-        child = node
-    end
-    return node
-end
-
 function findindexinlist(linkedlist, index)
     i = 1
     node = linkedlist
@@ -29,12 +10,3 @@ function findindexinlist(linkedlist, index)
     end
     return node.value
 end
-
-
-list = createlinkedlist(8)
-
-println(list)
-println()
-hei = findindexinlist(list, 3)
-println()
-println(hei)
