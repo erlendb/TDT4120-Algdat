d0b791321e78bc0db9e9b34b27b277c6a8293344
Erlend Blomseth <erlend@elendig.no>
2019-09-30 15:29:18 +0200

    oppg3 godkjent

--- oving3/bruteforce.jl
+++ oving3/bruteforce.jl
@@ -1,3 +1,23 @@
 function bruteforce(x)
-
+	minLength = Inf
+	for i in 1:size(x)[1]
+		#display(i)
+		for j in i+1:size(x)[1]
+			#display(j)
+			x1 = x[i, 1]
+			y1 = x[i, 2]
+			x2 = x[j, 1]
+			y2 = x[j, 2]
+			length = sqrt((x2-x1)^2+(y2-y1)^2)
+			#display(length)
+			if (length < minLength)
+				minLength = length
+			end
+		end
+	end
+	return minLength
 end
+
+a = [0.0 0.0; 1.0 1.0; 100.0 0.0]
+hei = bruteforce(a)
+display(hei)
